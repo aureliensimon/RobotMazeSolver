31bca185fadb9cf88bc802daa03763cee4e59cf4
aureliensimon <aureliensimon29260@gmail.com>
2019-06-23 17:55:22 +0200

    opti rotatestart

--- main.c
+++ main.c
@@ -17,19 +17,18 @@ int main(int argc, char**argv){
     // Init la Carte & Robot
     map = getmap(argv[1]);
     initRobot(&robot, map);
+    rotatestart(&robot, map);
 
     // Choix du mode
     if(!strcmp("t", argv[2])){
         // MODE TEXTUEL
         printmenu(argv[1], &robot);
-        rotatestart(&robot, map);
         int way = solvemaze(robot, map, graph, 't', &steps);
         if(!way){
             printf("Pas de Solution :(\n");
         }
     }else if(!strcmp("g", argv[2])){
         // MODE GRAPHIQUE
-        rotatestart(&robot, map);
         graphmode(graph, robot, map, &steps);
     }else{
         // Mauvais choix de mode
